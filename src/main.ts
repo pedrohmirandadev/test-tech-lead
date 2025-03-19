4135b1dd56f53540defa09401b2736d7956332c7
Pedro Miranda <pedrohmirandadev@gmail.com>
2025-03-19 16:38:04 -0300

    add swagger

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Teste Tech Lead")
+    .setDescription("Teste para Tech Lead")
+    .setVersion("1.0")
+    .addTag("teddy")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
